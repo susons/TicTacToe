d12e454859490a5b37c7e443e358145bdd236536
kristaps <kristapsbulssusons@gmail.com>
2021-03-10 21:16:07 +0200

    [fix] changed min value

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Input as SemanticInput } from 'semantic-ui-react';
 import './input.scss';
 
-export const Input = ({ placeholder, onChange, value, label, name }) => {
+export const Input = ({ placeholder, onChange, value, label, name, min }) => {
   return (
     <SemanticInput
       type='number'
-      min='1'
+      min={min}
       max='9'
       size='small'
       placeholder={placeholder}
--- src/components/molecules/Navigation.jsx
+++ src/components/molecules/Navigation.jsx
@@ -5,9 +5,9 @@ import { Input } from '../atoms/Input/Input';
 export const Navigation = ({ onChange, settings, onGenerate }) => {
   return (
     <div className="navigation">
-      <Input onChange={onChange} value={settings.rows} name="rows" label="Rows" />
-      <Input onChange={onChange} value={settings.columns} name="columns" label="Columns" />
-      <Input onChange={onChange} value={settings.winPoints} name="winPoints" label="Wictory points" />
+      <Input min="3" onChange={onChange} value={settings.rows} name="rows" label="Rows" />
+      <Input min="3" onChange={onChange} value={settings.columns} name="columns" label="Columns" />
+      <Input min="3" onChange={onChange} value={settings.winPoints} name="winPoints" label="Wictory points" />
       <Button primary onClick={onGenerate}>Generate game</Button>
     </div>
   )
